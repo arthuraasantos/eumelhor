4282d572eb2163b1f1677bec3096269508fbd603
emersonaraujo <mmsr_luis@hotmail.com>
2016-01-26 17:25:37 -0200

    Inclusão do cabeçalho na requisição da API

--- EuMelhor/EuMelhor.Api/Controllers/UserController.cs
+++ EuMelhor/EuMelhor.Api/Controllers/UserController.cs
@@ -7,10 +7,12 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Cors;
 using System.Web.Http.Description;
 
 namespace EuMelhor.Api.Controllers
 {
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
     public class UserController : ApiController
     {
         public UserAppService UserAppService;
